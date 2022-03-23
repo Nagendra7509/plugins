3145d286ab771e2ee0862b433cdca314d3bbc513
nagendra-pujari <nagendra.pujari1191@gmail.com>
2022-03-23 09:16:33 +0530

    added new changes

--- packages/video_player/video_player/lib/video_player.dart
+++ packages/video_player/video_player/lib/video_player.dart
@@ -565,6 +565,7 @@ class VideoPlayerController extends ValueNotifier<VideoPlayerValue> {
   ///
   /// [volume] indicates a value between 0.0 (silent) and 1.0 (full volume) on a
   /// linear scale.
+  /// linear scale.
   Future<void> setSomething(double volume) async {
     value = value.copyWith(volume: volume.clamp(0.0, 1.0));
     await _applyVolume();
